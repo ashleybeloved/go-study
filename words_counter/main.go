3c50ed0f93ca6f8a9587946b6d89a790d734e3a0
ashleybeloved <alphatripped+github@gmail.com>
2026-01-17 15:08:18 +0300

    fix

--- words_counter/main.go
+++ words_counter/main.go
@@ -7,12 +7,9 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	"unicode/utf8"
 )
 
 func main() {
-	// Читаем файл - переводим в строку
-
 	bytes, err := os.ReadFile("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -20,47 +17,28 @@ func main() {
 
 	text := string(bytes)
 
-	// Можно было бы сделать и так, но есть крутой пакет RegExp, который чистит всё одной строкой кода.
+	re := regexp.MustCompile(`\P{L}+`)
+	cleanText := re.ReplaceAllString(text, " ")
+	cleanText = strings.ToLower(cleanText)
 
-	// cleanText := strings.ReplaceAll(text, ",", "")
-	// cleanText = strings.ReplaceAll(cleanText, "!", "")
-	// cleanText = strings.ReplaceAll(cleanText, ".", "")
-	// cleanText = strings.ReplaceAll(cleanText, ";", "")
-
-	re := regexp.MustCompile(`[^\p{L}\s]+`) // [^\p{L}\s] - удалить всё, что НЕ является буквой любого алфавита и НЕ является пробелом
-	cleanText := re.ReplaceAllString(text, "")
-
-	words := strings.Split(cleanText, " ")
-	symbols := utf8.RuneCountInString(text)
+	words := strings.Fields(cleanText)
 
 	commonWords := make(map[string]int)
+
 	for _, word := range words {
-		if word != "" {
-			commonWords[word]++
-		}
+		commonWords[word]++
 	}
 
-	fmt.Printf("Stats:\n\n- Symbols: %v\n- Words: %v\n\nThe most common words:\n", symbols, len(words))
-
-	type wordStat struct {
-		word  string
-		count int
+	keys := make([]string, 0, len(commonWords))
+	for k := range commonWords {
+		keys = append(keys, k)
 	}
 
-	var stats []wordStat
-	for k, v := range commonWords {
-		stats = append(stats, wordStat{k, v})
-	}
-
-	// Сортируем слайс от большего к меньшему
-
-	sort.Slice(stats, func(i, j int) bool {
-		return stats[i].count > stats[j].count
+	sort.Slice(keys, func(i, j int) bool {
+		return commonWords[keys[i]] > commonWords[keys[j]]
 	})
 
-	// Вывод топ-5 слов
-
-	for i := 0; i < len(stats) && i < 5; i++ {
-		fmt.Printf("%d. %s: %d\n", i+1, stats[i].word, stats[i].count)
+	for _, k := range keys {
+		fmt.Printf("%s: %d\n", k, commonWords[k])
 	}
 }
